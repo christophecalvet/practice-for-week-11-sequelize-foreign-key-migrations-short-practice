440ccf28980364645fb5390fe0b7855bb1790ef7
Christophe Calvet <onlyatest@mytest.com>
2024-09-10 15:49:04 +0200

    Done

---
+++ server/db/migrations/20240910133703-create-musician-instrument.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('MusicianInstruments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      musicianId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model:'Musicians',
+            key: "id"
+        },
+        onDelete: 'cascade'
+      },
+      instrumentId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model:'Instruments',
+            key: "id"
+        },
+        onDelete: 'cascade'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('MusicianInstruments');
+  }
+};
